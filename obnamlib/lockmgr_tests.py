e12df6d36d1fe71430670d06fada355b49d66d47
David Fries <David@Fries.net>
2013-02-13 23:02:36 -0600

    get lockmgr_tests to pass now that lockmgr has been updated

--- obnamlib/lockmgr_tests.py
+++ obnamlib/lockmgr_tests.py
@@ -21,6 +21,17 @@ import unittest
 
 import obnamlib
 
+class FakeHookFS(object):
+    ''' A class to match the calling arguments of repo's HookedFS'''
+    def __init__(self, fs):
+        self.fs = fs
+
+    def __getattr__(self, name):
+        return getattr(self.fs, name)
+
+    # ignore runfilters, as FakeHookFS doesn't have any filters
+    def cat(self, filename, runfilters=True):
+        return self.fs.cat(filename)
 
 class LockManagerTests(unittest.TestCase):
 
@@ -35,12 +46,16 @@ class LockManagerTests(unittest.TestCase):
             dirname = os.path.join(self.tempdir, x)
             os.mkdir(dirname)
             self.dirnames.append(dirname)
-        self.fs = obnamlib.LocalFS(self.tempdir)
         self.timeout = 10
         self.now = 0
+        self.local_fs = obnamlib.LocalFS(self.tempdir)
+        self.fs = FakeHookFS(self.local_fs)
         self.lm = obnamlib.LockManager(self.fs, self.timeout, '')
         self.lm._time = self.fake_time
         self.lm._sleep = lambda: None
+        self.lm2 = obnamlib.LockManager(self.fs, self.timeout, '')
+        self.lm2._time = self.fake_time
+        self.lm2._sleep = lambda: None
 
     def tearDown(self):
         shutil.rmtree(self.tempdir)
@@ -59,13 +74,18 @@ class LockManagerTests(unittest.TestCase):
         self.assertFalse(self.lm.is_locked(self.dirnames[0]))
 
     def test_waits_until_timeout_for_locked_directory(self):
-        self.lm.lock([self.dirnames[0]])
+        self.lm2.lock([self.dirnames[0]])
         self.assertRaises(obnamlib.LockFail,
                           self.lm.lock, [self.dirnames[0]])
         self.assertTrue(self.now >= self.timeout)
 
-    def test_notices_when_preexisting_lock_goes_away(self):
+    def test_notices_double_lock(self):
         self.lm.lock([self.dirnames[0]])
+        self.assertRaises(obnamlib.LockFail,
+                          self.lm.lock, [self.dirnames[0]])
+
+    def test_notices_when_preexisting_lock_goes_away(self):
+        self.lm2.lock([self.dirnames[0]])
         self.lm._sleep = lambda: os.remove(
             self.lm._lockname(self.dirnames[0]))
         self.lm.lock([self.dirnames[0]])
@@ -86,5 +106,13 @@ class LockManagerTests(unittest.TestCase):
         self.lm.lock([self.dirnames[-1]])
         self.assertRaises(obnamlib.LockFail, self.lm.lock, self.dirnames)
         for dirname in self.dirnames[:-1]:
+            print("lock " + dirname)
             self.assertFalse(self.lm.is_locked(dirname))
         self.assertTrue(self.lm.is_locked(self.dirnames[-1]))
+
+    def test_lock_manager_with_left_over_lock(self):
+        # exercise the lock manager when there is a lock to cleanup
+        print("calling test_lock_manager_with_left_over_lock")
+        self.lm2.lock([self.dirnames[2]])
+        self.local_fs.close()
+        print("fs.close() called")
